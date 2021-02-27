0526dcb3f507f54068cc29fef92d9d88e2e6359d
Benjamin Bradley <benjamin@wegeekout.com>
2021-02-27 14:08:51 -0600

    remove extra debug

--- src/models/roles.js
+++ src/models/roles.js
@@ -6,12 +6,8 @@ async function getRoles() {
         console.log('entering getRoles');
         const sql = `SELECT SUBSTRING(COLUMN_TYPE,5) AS enumDef FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '${mysql.db_name}' AND TABLE_NAME = 'user_roles' AND COLUMN_NAME = 'role'`
         const row = await connection.query(sql);
-        console.log("row",row)
-        console.log("row.enumDef",row[0].enumDef)
         const enumArrayStr = row[0].enumDef.replace(/\(/g, '[').replace(/\)/g,']').replace(/'/g,'"')
-        console.log("enumArrayStr",enumArrayStr)
         const enumArray = JSON.parse(enumArrayStr)
-        console.log("enumArray",enumArray)
         return enumArray;
     } catch(err) {
         throw err;
